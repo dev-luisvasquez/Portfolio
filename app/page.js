aee89e520967647e274642610cb556e643617d74
dev-luisvasquez <luisvasquezcalistro99@gmail.com>
2025-08-04 20:32:24 -0400

    Descomentar GlowCard en Experience y ajustar importaciones en page.js para mejorar la legibilidad del código

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
-/* import Experience from "./components/homepage/experience"; */
+import Experience from "./components/homepage/experience";
 import Skills from "./components/homepage/skills";
 import Projects from "./components/homepage/projects";
 import ContactSection from "./components/homepage/contact";
@@ -11,7 +11,7 @@ export default async function Home() {
     <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
-  {/*     <Experience /> */}
+      <Experience />
       <Skills />
       <Projects /> 
       <ContactSection />
