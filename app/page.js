a7fe7440cf905dee70cc9510116f6f00a0f0ee30
dev-luisvasquez <luisvasquezcalistro99@gmail.com>
2025-08-04 20:13:39 -0400

    Eliminar secciones comentadas en page.js para simplificar el código

--- app/page.js
+++ app/page.js
@@ -1,22 +1,20 @@
-
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
+/* import AboutSection from "./components/homepage/about";
+import Experience from "./components/homepage/experience";
 import Skills from "./components/homepage/skills";
-import Footer from "./components/footer";
+import Projects from "./components/homepage/projects";
+import ContactSection from "./components/homepage/contact"; */
 
 export default async function Home() {
 
   return (
     <div suppressHydrationWarning>
       <HeroSection />
-      <AboutSection />
+      {/* <AboutSection />
       <Experience />
       <Skills />
       <Projects /> 
-      <ContactSection />
+      <ContactSection /> */}
     </div>
   )
 }
