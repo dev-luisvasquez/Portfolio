8070069f35a3e05a94ded4c46ea0acde927629e5
dev-luisvasquez <luisvasquezcalistro99@gmail.com>
2025-08-04 20:16:04 -0400

    Descomentar AboutSection en page.js y ajustar comentarios para mejorar la legibilidad del código

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "./components/homepage/hero-section";
-/* import AboutSection from "./components/homepage/about";
-import Experience from "./components/homepage/experience";
+import AboutSection from "./components/homepage/about";
+/* import Experience from "./components/homepage/experience";
 import Skills from "./components/homepage/skills";
 import Projects from "./components/homepage/projects";
 import ContactSection from "./components/homepage/contact"; */
@@ -10,8 +10,8 @@ export default async function Home() {
   return (
     <div suppressHydrationWarning>
       <HeroSection />
-      {/* <AboutSection />
-      <Experience />
+      <AboutSection />
+     {/*  <Experience />
       <Skills />
       <Projects /> 
       <ContactSection /> */}
