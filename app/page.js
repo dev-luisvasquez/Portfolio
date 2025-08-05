94961c2737f9df279c336d69fb3254f391a72abf
dev-luisvasquez <luisvasquezcalistro99@gmail.com>
2025-08-04 21:03:31 -0400

    Reemplazar importación de Experience por ExperienceClientWrapper en page.js para mejorar la funcionalidad del componente

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
-import Experience from "./components/homepage/experience";
+import ExperienceClientWrapper from "./components/ExperienceClientWrapper";
 import Skills from "./components/homepage/skills";
 import Projects from "./components/homepage/projects";
 import ContactSection from "./components/homepage/contact";
@@ -11,7 +11,7 @@ export default async function Home() {
     <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
-      <Experience />
+      <ExperienceClientWrapper />
       <Skills />
       <Projects /> 
       <ContactSection />
